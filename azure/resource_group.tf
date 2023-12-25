3dbaeb127027f9b8ff9c1e1888def0b252190579
michaeljguarino <mguarino46@gmail.com>
2023-12-24 21:08:15 -0500

    add forgotten resource group file

--- azure/resource_group.tf
+++ azure/resource_group.tf
@@ -1,4 +1,6 @@
 resource "azurerm_resource_group" "main" {
+  count = var.create_resource_group ? 1 : 0
+
   location = var.location
   name     = coalesce(var.resource_group_name, "${random_id.prefix.hex}-rg")
 }
