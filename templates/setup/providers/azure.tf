2ba6ed00e2128d5b25d458f3b9863fc12219ae5d
michaeljguarino <mguarino46@gmail.com>
2023-12-30 16:45:38 -0500

    configure region settings

--- templates/setup/providers/azure.tf
+++ templates/setup/providers/azure.tf
@@ -2,4 +2,5 @@ module "mgmt" {
     source              = "../bootstrap/terraform/clouds/azure"
     resource_group_name = "{{ .Project }}"
     cluster_name        = "{{ .Cluster }}"
+    location            = "{{ .Region }}"
 }
--- templates/setup/providers/gcp.tf
+++ templates/setup/providers/gcp.tf
@@ -2,4 +2,5 @@ module "mgmt" {
     source              = "../bootstrap/terraform/clouds/gcp"
     project_id          = "{{ .Project }}"
     cluster_name        = "{{ .Cluster }}"
+    region              = "{{ .Region }}"
 }
