7026b3c182fcf90e6c3e0b7dd7d95f842e86538e
michaeljguarino <mguarino46@gmail.com>
2024-01-02 18:10:40 -0500

    bump console chart

--- templates/setup/console.tf
+++ templates/setup/console.tf
@@ -30,7 +30,7 @@ resource "helm_release" "console" {
   namespace        = "plrl-console"
   chart            = "console"
   repository       = "https://pluralsh.github.io/console"
-  version          = "0.1.18"
+  version          = "0.1.19"
   create_namespace = true
   timeout          = 300
   values           = [
