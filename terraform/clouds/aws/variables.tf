79bdd0258ec95d0aaca4d72d05a5a2b27d6d7afa
Hans Rauer <h.w.rauer@googlemail.com>
2024-01-04 14:01:38 +0100

    fix subnet ranges

--- terraform/clouds/aws/variables.tf
+++ terraform/clouds/aws/variables.tf
@@ -35,7 +35,7 @@ variable "vpc_cidr" {
 
 variable "private_subnets" {
   type    = list(string)
-  default = ["10.0.1.0/20", "10.0.2.0/20", "10.0.3.0/20"]
+  default = ["10.1.0.0/20", "10.1.16.0/20", "10.1.32.0/20"]
 }
 
 variable "public_subnets" {
