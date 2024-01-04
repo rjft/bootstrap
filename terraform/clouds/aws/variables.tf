a3e43c3d360a363b0ccb0cf8f652b7a105c8d8e4
Hans Rauer <h.w.rauer@googlemail.com>
2024-01-04 19:28:27 +0100

    fix cidr blocks

--- terraform/clouds/aws/variables.tf
+++ terraform/clouds/aws/variables.tf
@@ -35,7 +35,7 @@ variable "vpc_cidr" {
 
 variable "private_subnets" {
   type    = list(string)
-  default = ["10.1.0.0/20", "10.1.16.0/20", "10.1.32.0/20"]
+  default = ["10.0.16.0/20", "10.0.32.0/20", "10.0.48.0/20"]
 }
 
 variable "public_subnets" {
