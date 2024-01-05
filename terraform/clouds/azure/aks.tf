81ce5e15b12f1811332577815079c4a27bef1b7a
michaeljguarino <mguarino46@gmail.com>
2024-01-05 11:55:41 -0500

    simplify node pool var a bit more

--- terraform/clouds/azure/aks.tf
+++ terraform/clouds/azure/aks.tf
@@ -2,13 +2,15 @@ module "aks" {
   source = "Azure/aks/azurerm"
   version = "7.5.0"
 
-  kubernetes_version  = var.kubernetes_version
-  cluster_name        = var.cluster_name
-  resource_group_name = local.resource_group.name
-  prefix              = var.cluster_name
-  os_disk_size_gb     = 60
-  sku_tier            = "Standard"
-  rbac_aad            = true
-  vnet_subnet_id      = azurerm_subnet.network.id
-  node_pools          = {for name, pool in var.node_pools : name => merge({vnet_subnet_id = azurerm_subnet.network.id}, pool)}
+  kubernetes_version   = var.kubernetes_version
+  cluster_name         = var.cluster_name
+  resource_group_name  = local.resource_group.name
+  prefix               = var.cluster_name
+  os_disk_size_gb      = 60
+  sku_tier             = "Standard"
+  rbac_aad             = true
+  vnet_subnet_id       = azurerm_subnet.network.id
+  node_pools           = {for name, pool in var.node_pools : name => merge({name = name}, pool)}
+  
+  role_based_access_control_enabled = true
 }
