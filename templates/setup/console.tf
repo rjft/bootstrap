58d920f25e0fe9f1cab0a77e869d3effb03f3396
michaeljguarino <mguarino46@gmail.com>
2024-01-05 16:19:53 -0500

    split cert manager/flux installation from runtime chart

--- templates/setup/console.tf
+++ templates/setup/console.tf
@@ -1,3 +1,43 @@
+data "local_sensitive_file" "certmanager" {
+  filename = "${path.module}/../helm-values/certmanager.yaml"
+}
+
+resource "helm_release" "certmanager" {
+  name             = "cert-manager"
+  namespace        = "cert-manager"
+  chart            = "cert-manager"
+  repository       = "https://charts.jetstack.io"
+  version          = "v1.13.3"
+  create_namespace = true
+  timeout          = 300
+  wait             = true
+  values           = [
+    data.local_sensitive_file.certmanager.content
+  ]
+
+  depends_on = [ module.mgmt.cluster ]
+}
+
+data "local_sensitive_file" "flux" {
+  filename = "${path.module}/../helm-values/flux.yaml"
+}
+
+resource "helm_release" "flux" {
+  name             = "flux"
+  namespace        = "flux"
+  chart            = "flux"
+  repository       = "https://fluxcd-community.github.io/helm-charts"
+  version          = "2.12.2"
+  create_namespace = true
+  timeout          = 300
+  wait             = false
+  values           = [
+    data.local_sensitive_file.flux.content
+  ]
+
+  depends_on = [ module.mgmt.cluster ]
+}
+
 data "local_sensitive_file" "runtime" {
   filename = "${path.module}/../helm-values/runtime.yaml"
 }
@@ -7,7 +47,7 @@ resource "helm_release" "runtime" {
   namespace        = "plural-runtime"
   chart            = "runtime"
   repository       = "https://pluralsh.github.io/bootstrap"
-  version          = "0.1.10"
+  version          = "0.1.11"
   create_namespace = true
   timeout          = 300
   wait             = false
@@ -15,7 +55,7 @@ resource "helm_release" "runtime" {
     data.local_sensitive_file.console.content
   ]
 
-  depends_on = [ module.mgmt.cluster ]
+  depends_on = [ module.mgmt.cluster, helm_release.certmanager, helm_release.flux ]
 }
 
 resource "null_resource" "console" {
