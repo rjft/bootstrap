6a1be4e24f80066ff7a906aad505c67e7e6ad43b
michaeljguarino <mguarino46@gmail.com>
2024-01-06 00:22:20 -0500

    add http probe info for nginx ingress

--- templates/setup/console.tf
+++ templates/setup/console.tf
@@ -1,5 +1,26 @@
+// leave this resource static, if you want to modify console values, do it in the generated helm values directly instead.
+resource "null_resource" "console" {
+  provisioner "local-exec" {
+    command = "plural cd control-plane-values --name {{ .Cluster }} --dsn \"${module.mgmt.db_url}\" --domain {{ .Subdomain }} --file console.yaml"
+    working_dir = "${path.module}/../helm-values"
+  }
+}
+
 data "local_sensitive_file" "certmanager" {
   filename = "${path.module}/../helm-values/certmanager.yaml"
+}
+
+data "local_sensitive_file" "flux" {
+  filename = "${path.module}/../helm-values/flux.yaml"
+}
+
+data "local_sensitive_file" "runtime" {
+  filename = "${path.module}/../helm-values/runtime.yaml"
+}
+
+data "local_sensitive_file" "console" {
+  filename = "${path.module}/../helm-values/console.yaml"
+  depends_on = [ null_resource.console ]
 }
 
 resource "helm_release" "certmanager" {
@@ -18,10 +39,6 @@ resource "helm_release" "certmanager" {
   depends_on = [ module.mgmt.cluster ]
 }
 
-data "local_sensitive_file" "flux" {
-  filename = "${path.module}/../helm-values/flux.yaml"
-}
-
 resource "helm_release" "flux" {
   name             = "flux"
   namespace        = "flux"
@@ -38,16 +55,12 @@ resource "helm_release" "flux" {
   depends_on = [ module.mgmt.cluster ]
 }
 
-data "local_sensitive_file" "runtime" {
-  filename = "${path.module}/../helm-values/runtime.yaml"
-}
-
 resource "helm_release" "runtime" {
   name             = "runtime"
   namespace        = "plural-runtime"
   chart            = "runtime"
   repository       = "https://pluralsh.github.io/bootstrap"
-  version          = "0.1.15"
+  version          = "0.1.16"
   create_namespace = true
   timeout          = 300
   wait             = false
@@ -58,18 +71,6 @@ resource "helm_release" "runtime" {
   depends_on = [ module.mgmt.cluster, helm_release.certmanager, helm_release.flux ]
 }
 
-resource "null_resource" "console" {
-  provisioner "local-exec" {
-    command = "plural cd control-plane-values --name {{ .Cluster }} --dsn \"${module.mgmt.db_url}\" --domain {{ .Subdomain }} --file console.yaml"
-    working_dir = "${path.module}/../helm-values"
-  }
-}
-
-data "local_sensitive_file" "console" {
-  filename = "${path.module}/../helm-values/console.yaml"
-  depends_on = [ null_resource.console ]
-}
-
 resource "helm_release" "console" {
   name             = "console"
   namespace        = "plrl-console"
@@ -77,7 +78,7 @@ resource "helm_release" "console" {
   repository       = "https://pluralsh.github.io/console"
   version          = "0.1.21"
   create_namespace = true
-  timeout          = 300
+  timeout          = 600
   wait             = true
   values           = [
     data.local_sensitive_file.console.content
