3e68c78c4ccec6aebefbeed82ff5a2e12cb836ce
michaeljguarino <mguarino46@gmail.com>
2024-01-05 20:32:06 -0500

    use new runtime chart

--- templates/setup/console.tf
+++ templates/setup/console.tf
@@ -47,7 +47,7 @@ resource "helm_release" "runtime" {
   namespace        = "plural-runtime"
   chart            = "runtime"
   repository       = "https://pluralsh.github.io/bootstrap"
-  version          = "0.1.12"
+  version          = "0.1.13"
   create_namespace = true
   timeout          = 300
   wait             = false
