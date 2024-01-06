d2402def540a4ac0644db379bf971ace9ab9eff5
michaeljguarino <mguarino46@gmail.com>
2024-01-05 23:35:47 -0500

    need to rewire cert manager namespace elsewhere too

--- templates/setup/console.tf
+++ templates/setup/console.tf
@@ -47,7 +47,7 @@ resource "helm_release" "runtime" {
   namespace        = "plural-runtime"
   chart            = "runtime"
   repository       = "https://pluralsh.github.io/bootstrap"
-  version          = "0.1.14"
+  version          = "0.1.15"
   create_namespace = true
   timeout          = 300
   wait             = false
