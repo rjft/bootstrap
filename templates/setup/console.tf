e45705fea59f55a98f5c2e2f76252de563b5f574
michaeljguarino <mguarino46@gmail.com>
2024-01-05 23:22:03 -0500

    another fix for cert manager certs

--- templates/setup/console.tf
+++ templates/setup/console.tf
@@ -47,7 +47,7 @@ resource "helm_release" "runtime" {
   namespace        = "plural-runtime"
   chart            = "runtime"
   repository       = "https://pluralsh.github.io/bootstrap"
-  version          = "0.1.13"
+  version          = "0.1.14"
   create_namespace = true
   timeout          = 300
   wait             = false
