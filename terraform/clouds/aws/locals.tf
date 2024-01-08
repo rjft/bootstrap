fdb2545425656c58fec0ea556d2f36f04a4cac28
michaeljguarino <mguarino46@gmail.com>
2024-01-07 21:51:00 -0500

    fix coalesce

--- terraform/clouds/aws/locals.tf
+++ terraform/clouds/aws/locals.tf
@@ -1,6 +1,6 @@
 locals {
   db_name = var.db_name == "" ? "${var.cluster_name}-plural-db" : var.db_name
-  db_url = format("postgresql://console:%s@%s:5432/console", random_password.password.result, coalesce(try(module.db.db_instance_address, ""), ""))
+  db_url = format("postgresql://console:%s@%s:5432/console", random_password.password.result, coalesce(try(module.db.db_instance_address, ""), "ignore"))
   cluster_ready = {
     cluster = module.eks
     addons = module.eks_blueprints_addons
