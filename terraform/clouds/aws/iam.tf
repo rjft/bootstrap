b683859f3f3380be922e479a15f559d03e0668f9
seemywings <kevin.jayne@icloud.com>
2024-05-06 13:05:32 -0400

    add vars for external dns and add to the iam policy

--- terraform/clouds/aws/iam.tf
+++ terraform/clouds/aws/iam.tf
@@ -1,11 +1,14 @@
 module "assumable_role_certmanager" {
-  source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version                       = "3.14.0"
-  create_role                   = true
-  role_name                     = "${var.cluster_name}-certmanager"
-  provider_url                  = replace(local.cluster_oidc_issuer_url, "https://", "")
-  role_policy_arns              = [aws_iam_policy.certmanager.arn]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:${var.namespace}:${var.certmanager_serviceaccount}"]
+  source           = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version          = "3.14.0"
+  create_role      = true
+  role_name        = "${var.cluster_name}-certmanager"
+  provider_url     = replace(local.cluster_oidc_issuer_url, "https://", "")
+  role_policy_arns = [aws_iam_policy.certmanager.arn]
+  oidc_fully_qualified_subjects = [
+    "system:serviceaccount:${var.externaldns_namespace}:${var.externaldns_serviceaccount}",
+    "system:serviceaccount:${var.namespace}:${var.certmanager_serviceaccount}"
+  ]
 }
 
 resource "aws_iam_policy" "certmanager" {
@@ -31,7 +34,7 @@ resource "aws_iam_policy" "certmanager" {
         },
         {
           "Effect": "Allow",
-          "Action": "route53:ListHostedZonesByName",
+          "Action": "route53:ListHostedZones",
           "Resource": "*"
         }
       ]
--- terraform/clouds/aws/variables.tf
+++ terraform/clouds/aws/variables.tf
@@ -118,3 +118,15 @@ variable "namespace" {
   type    = string
   default = "bootstrap"
 }
+
+variable "externaldns_serviceaccount" {
+  type        = string
+  default     = "externaldns"
+  description = "name of the external dns service account"
+}
+
+variable "externaldns_namespace" {
+  type        = string
+  default     = "plrl-runtime"
+  description = "name of the external dns namespace"
+}
